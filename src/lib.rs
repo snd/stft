a0f50e13f8c6b17b26f64ff66d5f3866dccc8a6e
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-02 22:25:17 +0100

    autoformat

--- src/lib.rs
+++ src/lib.rs
@@ -36,53 +36,47 @@ pub fn log10_positive<T: Float + Signed + Zero>(value: T) -> T {
     // Signed.is_negative
     // Zero.zero
     let log = value.log10();
-    if log.is_negative() { T::zero() } else { log }
+    if log.is_negative() {
+        T::zero()
+    } else {
+        log
+    }
 }
 
 impl<T: Float + Signed + FromPrimitive + CanRepresentPi> STFT<T> {
-    pub fn window_type_to_window_vec(
-        window_type: WindowType,
-        window_size: usize,
-    ) -> Option<Vec<T>> {
+    pub fn window_type_to_window_vec(window_type: WindowType,
+                                     window_size: usize)
+                                     -> Option<Vec<T>> {
         match window_type {
             WindowType::Hanning => Some(apodize::hanning_iter(window_size).collect::<Vec<T>>()),
             WindowType::Hamming => Some(apodize::hamming_iter(window_size).collect::<Vec<T>>()),
             WindowType::Blackman => Some(apodize::blackman_iter(window_size).collect::<Vec<T>>()),
             WindowType::Nuttall => Some(apodize::nuttall_iter(window_size).collect::<Vec<T>>()),
-            WindowType::None => None
+            WindowType::None => None,
         }
     }
 
-    pub fn new(
-        window_type: WindowType,
-        window_size: usize,
-        step_size: usize,
-        zero_padding: usize,
-    ) -> STFT<T> {
+    pub fn new(window_type: WindowType,
+               window_size: usize,
+               step_size: usize,
+               zero_padding: usize)
+               -> STFT<T> {
         let window = STFT::window_type_to_window_vec(window_type, window_size);
-        STFT::<T>::new_with_window(
-            window,
-            window_size,
-            step_size,
-            zero_padding,
-        )
+        STFT::<T>::new_with_window(window, window_size, step_size, zero_padding)
     }
 
     // TODO this should ideally take an iterator and not a vec
-    pub fn new_with_window(
-        window: Option<Vec<T>>,
-        window_size: usize,
-        step_size: usize,
-        zero_padding: usize,
-    ) -> STFT<T> {
+    pub fn new_with_window(window: Option<Vec<T>>,
+                           window_size: usize,
+                           step_size: usize,
+                           zero_padding: usize)
+                           -> STFT<T> {
         // TODO more assertions:
         // window_size is power of two
         // step_size > 0
         assert!(step_size <= window_size);
         let inverse = false;
-        let complex_zero = Complex::new(
-            T::from(0.).unwrap(),
-            T::from(0.).unwrap());
+        let complex_zero = Complex::new(T::from(0.).unwrap(), T::from(0.).unwrap());
         STFT {
             window_size: window_size,
             step_size: step_size,
@@ -90,11 +84,11 @@ impl<T: Float + Signed + FromPrimitive + CanRepresentPi> STFT<T> {
             sample_ring: SliceRingImpl::new(),
             window: window,
             complex_fft_input: std::iter::repeat(complex_zero)
-                .take(window_size)
-                .collect(),
+                                   .take(window_size)
+                                   .collect(),
             complex_fft_output: std::iter::repeat(complex_zero)
-                .take(window_size)
-                .collect()
+                                    .take(window_size)
+                                    .collect(),
         }
     }
 
