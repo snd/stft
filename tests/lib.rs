dc53c243fbc351cc4db47507d1b35ff8bb5970ba
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-03 00:01:23 +0100

    more testing

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,5 +5,12 @@ use stft::{STFT, WindowType};
 fn test_stft() {
     let mut stft = STFT::<f64>::new(WindowType::Hanning, 8, 4);
     assert!(!stft.can_compute());
-    assert_eq!(stft.output_size(), 512);
+    assert_eq!(stft.output_size(), 4);
+    assert_eq!(stft.len(), 0);
+    stft.feed(&vec![500., 0., 100.][..]);
+    assert_eq!(stft.len(), 3);
+    assert!(!stft.can_compute());
+    let mut output: Vec<f64> = vec![0.; 4];
+    stft.compute(&mut output[..]);
+    println!("{:?}", output);
 }
