e4eff054d09fb3d9a670fefa1faeefe646cdfa29
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-07 17:28:59 +0100

    add benchmark for 10 seconds audio

--- benches/lib.rs
+++ benches/lib.rs
@@ -60,9 +60,39 @@ fn bench_stft_compute_1024_f64(bencher: &mut test::Bencher) {
     bench_stft_compute!(bencher, 1024, f64);
 }
 
-// #[bench]
-// fn bench_stft_1_minute_audio(bencher: &mut test::Bencher) {
-//     $bencher.iter(|| {
-//         fft.process(&input[..], &mut output[..])
-//     });
-// }
+macro_rules! bench_stft_audio {
+    ($bencher:expr, $seconds:expr, $float:ty) => {{
+        // let's generate some fake audio
+        let sample_rate: usize = 44100;
+        let seconds: usize = $seconds;
+        let sample_count = sample_rate * seconds;
+        let all_samples = (0..sample_count).map(|x| x as $float).collect::<Vec<$float>>();
+        $bencher.iter(|| {
+            // let's initialize our short-time fourier transform
+            let window_type: WindowType = WindowType::Hanning;
+            let window_size: usize = 1024;
+            let step_size: usize = 512;
+            let mut stft = STFT::<$float>::new(window_type, window_size, step_size);
+            // we need a buffer to hold a computed column of the spectrogram
+            let mut spectrogram_column: Vec<$float> =
+                std::iter::repeat(0.).take(stft.output_size()).collect();
+            for some_samples in (&all_samples[..]).chunks(3000) {
+                stft.append_samples(some_samples);
+                while stft.contains_enough_to_compute() {
+                    stft.compute_column(&mut spectrogram_column[..]);
+                    stft.move_to_next_column();
+                }
+            }
+        });
+    }}
+}
+
+#[bench]
+fn bench_stft_10_seconds_audio_f32(bencher: &mut test::Bencher) {
+    bench_stft_audio!(bencher, 10, f32);
+}
+
+#[bench]
+fn bench_stft_10_seconds_audio_f64(bencher: &mut test::Bencher) {
+    bench_stft_audio!(bencher, 10, f64);
+}
